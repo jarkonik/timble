5fe49bc608cec5b02112ae9ee05453def029fe38
Jarosław Konik <jarkonik@gmail.com>
2013-11-07 14:41:01 +0100

    Cleanup

--- timble.rb
+++ timble.rb
@@ -3,28 +3,19 @@
 require_relative 'gui.rb'
 require_relative 'main.rb'
 
-week = Week.new
-lesson = Lesson.new
-lesson[:name]="Test"
-lesson[:building]="b2"
-week.days[:saturday].addlesson(lesson)
-lesson = Lesson.new
-lesson[:name]="Test"
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.days[:wednesday].addlesson(lesson)
-week.save 'test.db'
+
+#TESTING:
+#week = Week.new
+#lesson = Lesson.new
+#lesson[:name]="Test"
+#lesson[:building]="b2"
+#week.days[:saturday].addlesson(lesson)
+#week.save 'test.db'
+
 Gtk.init
   window = Interface.new
   window.updatelessons(week)
   window.clearlessons
   window.updatelessons(week)
-
   window.show_all
 Gtk.main
